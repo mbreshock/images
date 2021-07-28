e0adc971db8d92f753220fc5de22d2391a25c608
mbreshock <mbresho1@jh.edu>
2021-07-28 16:18:56 -0700

    created gifs

---
+++ OCSdata/simpler_data_gif.R
@@ -0,0 +1,15 @@
+library(magick)
+# reading in each image
+step1 = image_read("~/Desktop/Michael/images/OCSdata/OCS_data_folder.png")
+step2 = image_read("~/Desktop/Michael/images/OCSdata/data_folder.png")
+step3 = image_read("~/Desktop/Michael/images/OCSdata/simpler_data_folder.png")
+step4 = image_read("~/Desktop/Michael/images/OCSdata/simpler_data_files.png")
+
+# making vector of each image
+img = c(step1, step2, step3, step4, step4)
+
+# animating
+simpler_gif =  image_animate(img, delay = 100, optimize = TRUE)
+
+# saving to file
+image_write(simpler_gif,"~/Desktop/Michael/images/OCSdata/simpler_data_directory.gif")
---
+++ OCSdata/wrangled_rda_gif.R
@@ -0,0 +1,15 @@
+library(magick)
+# reading in each image
+step1 = image_read("~/Desktop/Michael/images/OCSdata/OCS_data_folder.png")
+step2 = image_read("~/Desktop/Michael/images/OCSdata/data_folder.png")
+step3 = image_read("~/Desktop/Michael/images/OCSdata/wrangled_data_folder.png")
+step4 = image_read("~/Desktop/Michael/images/OCSdata/wrangled_rda_files.png")
+
+# making vector of each image
+img = c(step1, step2, step3, step4, step4)
+
+# animating
+rda_gif =  image_animate(img, delay = 100, optimize = TRUE)
+
+# saving to file
+image_write(rda_gif,"~/Desktop/Michael/images/OCSdata/wrangled_rda_directory.gif")
